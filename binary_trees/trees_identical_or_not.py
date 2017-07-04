aab93217b2521ec72e34c802ac29dd82654fa42a
Manikanta Tummalapenta <manikanta.t@GGN000022.local>
2017-07-04 19:37:19 +0530

    Changing the name of Directory
    Codes Added:
    1. Determine if Two Trees are Identical or not (Recursive and Iterative)

---
+++ binary_trees/trees_identical_or_not.py
@@ -0,0 +1,98 @@
+'''
+
+Two trees are identical when they have same data and arrangement of data is
+also same. To identify if two trees are identical, we need to traverse both
+trees simultaneously, and while traversing we need to compare data and
+children of the trees.
+
+Recursive:http://www.geeksforgeeks.org/write-c-code-to-determine-if-two-trees-are-identical/
+Iterative:http://www.geeksforgeeks.org/iterative-function-check-two-trees-identical/
+
+'''
+
+
+class Node:
+    def __init__(self, value):
+        self.data = value
+        self.left = None
+        self.right = None
+
+
+def identical_or_not_recursive(a, b):
+    if(a is None and b is None):
+        return True
+
+    if(a is None or b is None):
+        return False
+
+    return (a.data == b.data and
+            identical_or_not_recursive(a.left, b.left) and
+            identical_or_not_recursive(a.right, b.right))
+
+
+def identical_or_not_iterative(a, b):
+    if(a is None and b is None):
+        return True
+
+    if(a is None or b is None):
+        return False
+
+    queue1 = []
+    queue2 = []
+    queue1.append(a)
+    queue2.append(b)
+
+    while(len(queue1) > 0 and len(queue2) > 0):
+        n1 = queue1.pop()
+        n2 = queue2.pop()
+
+        if(n1.data != n2.data):
+            return False
+
+        if(n1.left is not None and n2.left is not None):
+            queue1.append(n1.left)
+            queue2.append(n2.left)
+        elif(n1.left is None or n2.left is None):
+            return False
+
+        if(n1.right is not None and n2.right is not None):
+            queue1.append(n1.right)
+            queue2.append(n2.right)
+        elif(n1.right is None or n2.right is None):
+            return False
+
+    return True
+
+
+if __name__ == "__main__":
+    root1 = Node(1)
+    root1.left = Node(2)
+    root1.right = Node(3)
+    root1.left.left = Node(4)
+    root1.left.right = Node(5)
+
+    root2 = Node(1)
+    root2.left = Node(2)
+    root2.right = Node(3)
+    root2.left.left = Node(4)
+    root2.left.right = Node(5)
+
+    print("1. Recursive check: ", identical_or_not_recursive(root1, root2))
+    print("1. Iterative check: ", identical_or_not_iterative(root1, root2))
+
+    root3 = Node(1)
+    root3.left = Node(2)
+    root3.right = Node(3)
+    root3.left.left = Node(4)
+    root3.left.right = Node(6)
+
+    print("2. Recursive check: ", identical_or_not_recursive(root1, root3))
+    print("2. Iterative check: ", identical_or_not_iterative(root1, root3))
+
+    root4 = Node(1)
+    root4.left = Node(2)
+    root4.right = Node(3)
+    root4.left.left = Node(4)
+
+    print("3. Recursive check: ", identical_or_not_recursive(root1, root4))
+    print("3. Iterative check: ", identical_or_not_iterative(root1, root4))
